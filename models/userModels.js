0e950f333f98f877880866a001bc22a8b0d454c8
AbdulazizAhmedAj <96697853+AbdulazizAhmedAj@users.noreply.github.com>
2024-05-23 16:03:32 +0300

    Add files via upload

---
+++ models/userModels.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+
+
+
+const userschema = new mongoose.Schema({
+    fullname: {
+        type: String
+    },
+    email: {
+        type: String,
+    }, 
+    password: {
+        type: String,
+    }, 
+}, { timestamps: true });
+
+const User = mongoose.model('User', userschema);
+module.exports = User
